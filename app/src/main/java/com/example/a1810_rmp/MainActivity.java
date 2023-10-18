bdf1867f80fd2cfdedaa14e05748f17eeae74f79
Cybercriminal <102804768+NomokonovD@users.noreply.github.com>
2023-10-18 14:59:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a1810_rmp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a1810_rmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
